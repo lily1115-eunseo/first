5cd556704244eb0d766a2fc896f9341225b45cd2
배은서 <lilybae1115@gmail.com>
2024-07-29 21:45:31 +0900

    level 1-(1)

--- java 개인 과제/src/main/java/calculator/App.java
+++ java 개인 과제/src/main/java/calculator/App.java
@@ -1,8 +1,15 @@
 package calculator;
 
-public class App {
+import java.util.Scanner;
 
+public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
 
     }
 }
